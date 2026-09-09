fabiocicerchia/llm-fit

feat(packaging): man page, OS packages and a staged install
Three things distro packaging needs and this repo did not have:

* man/<tool>.1 — committed roff, not generated at build time, so a
  cross-compiling packager does not have to run the binary to get it.
* .goreleaser.yaml — the same build that already produced archives now
  also produces deb, rpm, apk and Arch packages via nfpm, with the
  binary in /usr/bin and the man page in /usr/share/man/man1.
* make install honours DESTDIR and PREFIX. With PREFIX unset it still
  does what it did before, so the developer habit is unchanged; with
  PREFIX set it stages a tree, which is what `make install
  DESTDIR=$pkgdir PREFIX=/usr` in an APKBUILD or a spec file expects.

The release is cut as a draft and published last: immutable releases
reject asset uploads once a release is published.

Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_01PJDSbimNtVbucZBkcswNDB

Makefile
@@ -48,8 +48,31 @@ clean:
 setup: ## Install the pre-commit hook
 	pre-commit install
 
-install: ## Install the binary into GOBIN
-	go install ./...
+## install: install the binary and its man page; PREFIX=/usr/local for a system path
+install:
+ifeq ($(strip $(PREFIX)),)
+	go install $(PKG)
+	install -d "$(USER_MANDIR)"
+	install -m 0644 man/$(BINARY).1 "$(USER_MANDIR)/$(BINARY).1"
+	@dir="$$(go env GOBIN)"; [ -n "$$dir" ] || dir="$$(go env GOPATH)/bin"; \
+		echo "installed $$dir/$(BINARY) and $(USER_MANDIR)/$(BINARY).1"
+else
+	@$(MAKE) build
+	install -d "$(DESTDIR)$(PREFIX)/bin" "$(DESTDIR)$(PREFIX)/share/man/man1"
+	install -m 0755 $(BIN_DIR)/$(BINARY) "$(DESTDIR)$(PREFIX)/bin/$(BINARY)"
+	install -m 0644 man/$(BINARY).1 "$(DESTDIR)$(PREFIX)/share/man/man1/$(BINARY).1"
+	@echo "installed $(DESTDIR)$(PREFIX)/bin/$(BINARY)"
+endif
+
+## uninstall: remove what `make install` put down
+uninstall:
+ifeq ($(strip $(PREFIX)),)
+	@dir="$$(go env GOBIN)"; [ -n "$$dir" ] || dir="$$(go env GOPATH)/bin"; \
+		rm -f "$$dir/$(BINARY)" "$(USER_MANDIR)/$(BINARY).1"
+else
+	rm -f "$(DESTDIR)$(PREFIX)/bin/$(BINARY)" \
+		"$(DESTDIR)$(PREFIX)/share/man/man1/$(BINARY).1"
+endif
 
 run: ## Run the binary
 	go run ./cmd/llm-fit $(ARGS)

fabiocicerchia/chicco

Makefile
@@ -31,8 +31,31 @@ build: ## Compile the binary into ./bin
 	go build -ldflags "$(LDFLAGS)" -o bin/$(BIN) ./cmd/chicco
 
 .PHONY: install
-install: ## go install into GOBIN
+## install: install the binary and its man page; PREFIX=/usr/local for a system path
+install:
+ifeq ($(strip $(PREFIX)),)
 	go install -ldflags "$(LDFLAGS)" ./cmd/chicco
+	install -d "$(USER_MANDIR)"
+	install -m 0644 man/$(BIN).1 "$(USER_MANDIR)/$(BIN).1"
+	@dir="$$(go env GOBIN)"; [ -n "$$dir" ] || dir="$$(go env GOPATH)/bin"; \
+		echo "installed $$dir/$(BIN) and $(USER_MANDIR)/$(BIN).1"
+else
+	@$(MAKE) build
+	install -d "$(DESTDIR)$(PREFIX)/bin" "$(DESTDIR)$(PREFIX)/share/man/man1"
+	install -m 0755 bin/$(BIN) "$(DESTDIR)$(PREFIX)/bin/$(BIN)"
+	install -m 0644 man/$(BIN).1 "$(DESTDIR)$(PREFIX)/share/man/man1/$(BIN).1"
+	@echo "installed $(DESTDIR)$(PREFIX)/bin/$(BIN)"
+endif
+
+## uninstall: remove what `make install` put down
+uninstall:
+ifeq ($(strip $(PREFIX)),)
+	@dir="$$(go env GOBIN)"; [ -n "$$dir" ] || dir="$$(go env GOPATH)/bin"; \
+		rm -f "$$dir/$(BIN)" "$(USER_MANDIR)/$(BIN).1"
+else
+	rm -f "$(DESTDIR)$(PREFIX)/bin/$(BIN)" \
+		"$(DESTDIR)$(PREFIX)/share/man/man1/$(BIN).1"
+endif
 
 .PHONY: run
 run: ## Run chicco (pass args with ARGS="...")